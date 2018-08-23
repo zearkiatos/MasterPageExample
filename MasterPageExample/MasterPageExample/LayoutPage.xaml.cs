3f68069378127686dfbb71baa63fb12d1a386a7d
Pedro Capriles <pedro.capriles@arkanosoft.com>
2018-08-23 01:27:02 -0300

    Add project files.

---
+++ MasterPageExample/MasterPageExample/LayoutPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterPageExample
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LayoutPage : ContentPage
+	{
+        private View mainContent;
+
+        public View MainContent
+        {
+            get { return mainContent; }
+            set {
+                if(mainContent != value)
+                {
+                    mainContent = value;
+                    MainContentContainer.Children.Add(mainContent);
+                    OnPropertyChanged("MainContent");
+                }
+
+            }
+        }
+
+        public LayoutPage ()
+		{
+			InitializeComponent ();
+		}
+
+        
+	}
+}
---
+++ MasterPageExample/MasterPageExample/MasterPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using MasterPageExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterPageExample
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MasterPage : ContentPage
+	{
+
+        public ListView ListView
+        {
+            get { return listView; }
+        }
+
+        public MasterPage ()
+		{
+			InitializeComponent ();
+
+            var masterPageItem = new List<MasterPageItem>();
+            masterPageItem.Add(new MasterPageItem {
+                Title = "HomePage",
+                IconSource = "icon.png",
+                TargetType = typeof(HomePage)
+            });
+            masterPageItem.Add(new MasterPageItem
+            {
+                Title = "CSSPage",
+                IconSource = "ic_action_css.png",
+                TargetType = typeof(CSSPage)
+            });
+            masterPageItem.Add(new MasterPageItem
+            {
+                Title = "JavascriptPage",
+                IconSource = "ic_action_javascript.png",
+                TargetType = typeof(JavascriptPage)
+            });
+            masterPageItem.Add(new MasterPageItem
+            {
+                Title = "HtmlPage",
+                IconSource = "ic_action_html.png",
+                TargetType = typeof(HtmlPage)
+            });
+
+            listView.ItemsSource = masterPageItem;
+        }
+	}
+}
